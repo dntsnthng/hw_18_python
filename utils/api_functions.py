74d06f8dbf4147d429447a03a5634876e6a7d8ca
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-23 20:04:51 +0400

    finish 2.0

--- utils/api_functions.py
+++ utils/api_functions.py
@@ -1,11 +1,11 @@
 import requests
 
-from utils.helpers import helpers
+from utils.helpers import response_logging, response_attaching
 
 
 def api_request(url, endpoint, method, data=None, params=None, allow_redirects=None):
     url = f"{url}{endpoint}"
     response = requests.request(method, url, data=data, params=params, allow_redirects=allow_redirects)
-    helpers.response_logging(response)
-    helpers.response_attaching(response)
+    response_logging(response)
+    response_attaching(response)
     return response
